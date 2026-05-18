adf2e10ce2662af9d82f91ca3a90b2ff876fa1bd
Ozkar <ozcodx@gmail.com>
2026-05-18 13:00:18 -0500

    Add new recipe for crafting clay wet using smoker station type

--- crafts/smoker.lua
+++ crafts/smoker.lua
@@ -9,6 +9,14 @@ crafting.register_recipe({
 })
 
 crafting.register_recipe({
+	type = smoker_station_type,
+	output = "nodes_nature:clay_wet 5",
+	items = { "exile_improved_cooking:smoker_unfired 1" },
+	level = 1,
+	always_known = true,
+})
+
+crafting.register_recipe({
 	type = "mixing_spot",
 	output = "nodes_nature:clay 5",
 	items = { "exile_improved_cooking:smoker_unfired 1" },
